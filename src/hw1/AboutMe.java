af7407d3f98e672b73b5111d5940cd0600212a1f
mihtri92 <htmtri@protonmail.com>
2018-10-19 00:51:41 -0400

    w1 done

---
+++ src/hw1/AboutMe.java
@@ -0,0 +1,39 @@
+package hw1;
+
+public class AboutMe {
+	
+	//init fields
+	String name;
+	String school;
+	int age;
+	
+	//Constructor
+	AboutMe(String name,String school, int age){
+		this.name = name;
+		this.school = school;
+		this.age = age;
+	}
+	
+	//Methods
+	public String myName() {
+		return name;
+	}
+	
+	public String mySchool() {
+		return school;
+	}
+	
+	public int myAge() {
+		return age;
+	}
+	
+	public static void main(String[] args) {
+		AboutMe me = new AboutMe("Phil", "Purdue University", 36);
+		
+		System.out.println("My name is "+ me.myName() + 
+				",and I attend "+ me.mySchool() + 
+				". I am "+ me.myAge() + " years old");
+		
+	}
+
+}
