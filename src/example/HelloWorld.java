d0b770bd632dcd645acc37f70039ce6bd5ed9ff2
mihtri92 <htmtri@protonmail.com>
2018-10-20 01:28:07 -0400

    Update src/example/HelloWorld.java

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,8 +1,25 @@
 package example;
+import java.lang.Math;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        double x = Math.cos(Math.PI);
+        System.out.println(x);
+        double x2 = 8/3;
+        System.out.println(x2);
+        double x3 = 9.0/2;
+        System.out.println(x3);
+        final double GRAVITY = 9.81;
+        double z = Math.sqrt(GRAVITY);
+        System.out.println(z);
+        double y = 5.0/1.0+2.0*3.0;
+        System.out.println(y);
+        int a = 6,b = 10;
+        float c = (float)a/(float)b;
+        System.out.println(c);
+        
+        
     }
     
 }
