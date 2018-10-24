d990a85f0dca9419d4c2740fa3ef300d3df61572
mihtri92 <htmtri@protonmail.com>
2018-10-24 17:19:25 -0400

    Update src/example/HelloWorld.java

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -4,21 +4,23 @@ import java.lang.Math;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        double x = Math.cos(Math.PI);
-        System.out.println(x);
+        /*double x1 = Math.cos(Math.PI);
+        System.out.println(x1);
         double x2 = 8/3;
         System.out.println(x2);
         double x3 = 9.0/2;
         System.out.println(x3);
         final double GRAVITY = 9.81;
-        double z = Math.sqrt(GRAVITY);
-        System.out.println(z);
-        double y = 5.0/1.0+2.0*3.0;
-        System.out.println(y);
-        int a = 6,b = 10;
-        float c = (float)a/(float)b;
-        System.out.println(c);
-        
+        double z1 = Math.sqrt(GRAVITY);
+        System.out.println(z1);
+        double y1 = 5.0/1.0+2.0*3.0;
+        System.out.println(y1);
+        int a1 = 6,b1 = 10;
+        float c1 = (float)a1/(float)b1;
+        System.out.println(c1);
+        int a2 = Integer.MAX_VALUE;
+        System.out.println(a2);
+        */
     }
     
 }
